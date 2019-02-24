b3837e36e740ded774ad396b5df71a859c025304
Kotaro Yamazaki <k_yamazaki@kde.cs.tsukuba.ac.jp>
2019-02-24 16:41:29 +0900

    remove waste comments

--- Makefile
+++ Makefile
@@ -1,17 +1,19 @@
 #program name and objects file name
 PROGRAM = rankclus.out
-OBJS = main.o ranking.o graph.o clustering.o
+OBJS = main.o graph.o clustering.o
 OPTIMIZE = -O2
 #define macro
 CXX = clang++
 CXXFLAG = $(OPTIMIZE) -std=c++11
 
-all:real
-real: $(OBJS)
-	$(CXX) $(CXXFLAG) $(OBJS) -o $(PROGRAM)
+all:authority
+authority: $(OBJS) ranking.o
+	$(CXX) $(CXXFLAG) $(OBJS) ranking.o -o $(PROGRAM)
+pagerank: $(OBJS) single_pagerank.o
+	$(CXX) $(CXXFLAG) $(OBJS) single_pagerank.o -o $(PROGRAM)
 .cpp.o:
 	$(CXX) $(CXXFLAG) -c $< -Wall
 *.o: graph.hpp
 .PHONY: clean
 clean:
-	$(RM) *.o
+	$(RM) *.o *test*
--- single_pagerank.cpp
+++ single_pagerank.cpp
@@ -44,6 +44,7 @@ void single_pagerank(graph& g, int clusterNum){
                 }
                 RxSum += tmp;
                 g[*i].rx = tmp;
+
             }else{
                 for (auto e = out_edges(*i, g); e.first!=e.second; e.first++) {
                     // ノード *i の入エッジの重み（g[*e.first].weight）と
@@ -57,9 +58,9 @@ void single_pagerank(graph& g, int clusterNum){
                 RySum += tmp;
                 g[*i].ry = tmp;
             }
-//            cout << tmp << endl;
         }
     }
+
     for (boost::tie(i, j) = vertices(g); i!=j; i++) {
             if(*i < xNum){
                 g[*i].rx /= RxSum;
@@ -67,70 +68,4 @@ void single_pagerank(graph& g, int clusterNum){
                 g[*i].ry /= RySum;
             }
         }
-}
-
-void authority_ranking(graph& g, int clusterNum){
-    vertex_iterator i, j;
-    //iterator を用いて最初のiterから最後まで回す
-
-    //simple ranking
-    for (boost::tie(i, j) = vertices(g); i!=j; i++) {
-        double tmp = 0;
-        //出る全てのエッジに対してループを回す
-        if(*i < xNum){
-            for (auto e = in_edges(*i, g); e.first!=e.second; e.first++) {
-                // ノード *i の入エッジの重み（g[*e.first].weight）と
-                // そのエッジの元ノード（source(*e.first, g) のランク値（g[source(*e.first, g)].previous_rank）をかける
-                tmp += g[*e.first].weight;
-            }
-            g[*i].rx = tmp/WkXY_sum[clusterNum];
-        }else{
-             for (auto e = out_edges(*i, g); e.first!=e.second; e.first++) {
-                // ノード *i の入エッジの重み（g[*e.first].weight）と
-                // そのエッジの元ノード（source(*e.first, g) のランク値（g[source(*e.first, g)].previous_rank）をかける
-                if(g[target(*e.first, g)].label == "target")tmp += g[*e.first].weight;
-            }
-            g[*i].ry = tmp/WkXY_sum[clusterNum];
-        }
-    }
-
-    //authority ranking
-    for(int q = 0; q < rankiter; q++){
-        double RxSum = 0;
-        double RySum = 0;
-        for (boost::tie(i, j) = vertices(g); i!=j; i++) {
-            double tmp = 0;
-            //出る全てのエッジに対してループを回す
-            if(*i < xNum){
-                for (auto e = in_edges(*i, g); e.first!=e.second; e.first++) {
-                    // ノード *i の入エッジの重み（g[*e.first].weight）と
-                    // そのエッジの元ノード（source(*e.first, g) のランク値（g[source(*e.first, g)].previous_rank）をかける
-                    tmp += g[*e.first].weight * g[source(*e.first, g)].ry;
-                }
-                RxSum += tmp;
-                g[*i].rx = tmp;
-//                if(tmp != 0)cout << tmp << endl;
-            }else{
-                for (auto e = out_edges(*i, g); e.first!=e.second; e.first++) {
-                    // ノード *i の入エッジの重み（g[*e.first].weight）と
-                    // そのエッジの元ノード（source(*e.first, g) のランク値（g[source(*e.first, g)].previous_rank）をかける
-                    if(g[target(*e.first, g)].label == "target"){
-                        tmp += alpha*(g[*e.first].weight * g[target(*e.first, g)].rx);
-                    }else{
-                        tmp += (1-alpha)*(g[*e.first].weight * g[source(*e.first, g)].ry);
-                    }
-                }
-                RySum += tmp;
-                g[*i].ry = tmp;
-            }
-        }
-
-        for (boost::tie(i, j) = vertices(g); i!=j; i++) {
-            if(*i < xNum){
-                g[*i].rx /= RxSum;
-            }else{
-                g[*i].ry /= RySum;
-            }
-        }
-    }
 }
