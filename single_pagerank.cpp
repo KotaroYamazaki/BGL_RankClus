df27e1a59ccfd5d5b25c2240495318b3f5c13529
Kotaro Yamazaki <k_yamazaki@kde.cs.tsukuba.ac.jp>
2019-02-24 09:21:28 +0900

    maybe go it

---
+++ single_pagerank.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/compressed_sparse_row_graph.hpp>
+#include "graph.hpp"
+using namespace std;
+extern vector<int> WkXY_sum;
+extern int xNum;
+const double alpha = 0.95;
+const int rankiter = 15;
+
+void single_pagerank(graph& g, int clusterNum);
+void authority_ranking(graph& g, int clusterNum);
+
+void ranking(graph& subgraph, int clusterNum){
+    single_pagerank(subgraph, clusterNum);
+    //authority_ranking(subgraph, clusterNum);
+}
+
+void single_pagerank(graph& g, int clusterNum){
+    vertex_iterator i,j;
+
+    for (boost::tie(i, j) = vertices(g); i!=j; i++) {
+        //出る全てのエッジに対してループを回す
+        if(*i > xNum){
+            g[*i].ry = 1;
+        }
+    }
+
+    //authority ranking
+    double RxSum, RySum;
+    for(int q = 0; q < rankiter; q++){
+        RxSum = 0;
+        RySum = 0;
+        for (boost::tie(i, j) = vertices(g); i!=j; i++) {
+            double tmp = 0;
+            //出る全てのエッジに対してループを回す
+            if(*i < xNum){
+                for (auto e = in_edges(*i, g); e.first!=e.second; e.first++) {
+                    // ノード *i の入エッジの重み（g[*e.first].weight）と
+                    // そのエッジの元ノード（source(*e.first, g) のランク値（g[source(*e.first, g)].previous_rank）をかける
+                    tmp += g[*e.first].weight * g[source(*e.first, g)].ry;
+                }
+                RxSum += tmp;
+                g[*i].rx = tmp;
+            }else{
+                for (auto e = out_edges(*i, g); e.first!=e.second; e.first++) {
+                    // ノード *i の入エッジの重み（g[*e.first].weight）と
+                    // そのエッジの元ノード（source(*e.first, g) のランク値（g[source(*e.first, g)].previous_rank）をかける
+                    if(g[target(*e.first, g)].label == "target"){
+                        tmp += (g[*e.first].weight * g[target(*e.first, g)].rx);
+                    }else{
+                        tmp += (g[*e.first].weight * g[source(*e.first, g)].ry);
+                    }
+                }
+                RySum += tmp;
+                g[*i].ry = tmp;
+            }
+//            cout << tmp << endl;
+        }
+    }
+    for (boost::tie(i, j) = vertices(g); i!=j; i++) {
+            if(*i < xNum){
+                g[*i].rx /= RxSum;
+            }else{
+                g[*i].ry /= RySum;
+            }
+        }
+}
+
+void authority_ranking(graph& g, int clusterNum){
+    vertex_iterator i, j;
+    //iterator を用いて最初のiterから最後まで回す
+
+    //simple ranking
+    for (boost::tie(i, j) = vertices(g); i!=j; i++) {
+        double tmp = 0;
+        //出る全てのエッジに対してループを回す
+        if(*i < xNum){
+            for (auto e = in_edges(*i, g); e.first!=e.second; e.first++) {
+                // ノード *i の入エッジの重み（g[*e.first].weight）と
+                // そのエッジの元ノード（source(*e.first, g) のランク値（g[source(*e.first, g)].previous_rank）をかける
+                tmp += g[*e.first].weight;
+            }
+            g[*i].rx = tmp/WkXY_sum[clusterNum];
+        }else{
+             for (auto e = out_edges(*i, g); e.first!=e.second; e.first++) {
+                // ノード *i の入エッジの重み（g[*e.first].weight）と
+                // そのエッジの元ノード（source(*e.first, g) のランク値（g[source(*e.first, g)].previous_rank）をかける
+                if(g[target(*e.first, g)].label == "target")tmp += g[*e.first].weight;
+            }
+            g[*i].ry = tmp/WkXY_sum[clusterNum];
+        }
+    }
+
+    //authority ranking
+    for(int q = 0; q < rankiter; q++){
+        double RxSum = 0;
+        double RySum = 0;
+        for (boost::tie(i, j) = vertices(g); i!=j; i++) {
+            double tmp = 0;
+            //出る全てのエッジに対してループを回す
+            if(*i < xNum){
+                for (auto e = in_edges(*i, g); e.first!=e.second; e.first++) {
+                    // ノード *i の入エッジの重み（g[*e.first].weight）と
+                    // そのエッジの元ノード（source(*e.first, g) のランク値（g[source(*e.first, g)].previous_rank）をかける
+                    tmp += g[*e.first].weight * g[source(*e.first, g)].ry;
+                }
+                RxSum += tmp;
+                g[*i].rx = tmp;
+//                if(tmp != 0)cout << tmp << endl;
+            }else{
+                for (auto e = out_edges(*i, g); e.first!=e.second; e.first++) {
+                    // ノード *i の入エッジの重み（g[*e.first].weight）と
+                    // そのエッジの元ノード（source(*e.first, g) のランク値（g[source(*e.first, g)].previous_rank）をかける
+                    if(g[target(*e.first, g)].label == "target"){
+                        tmp += alpha*(g[*e.first].weight * g[target(*e.first, g)].rx);
+                    }else{
+                        tmp += (1-alpha)*(g[*e.first].weight * g[source(*e.first, g)].ry);
+                    }
+                }
+                RySum += tmp;
+                g[*i].ry = tmp;
+            }
+        }
+
+        for (boost::tie(i, j) = vertices(g); i!=j; i++) {
+            if(*i < xNum){
+                g[*i].rx /= RxSum;
+            }else{
+                g[*i].ry /= RySum;
+            }
+        }
+    }
+}
