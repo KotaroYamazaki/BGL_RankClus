d350c3967eadd830c7a40124215a53bf2213747f
Kotaro Yamazaki <k_yamazaki@kde.cs.tsukuba.ac.jp>
2019-05-31 14:59:02 +0900

    delete waste files

--- src/clustering.cpp
+++ src/clustering.cpp
@@ -1,5 +1,6 @@
 #include"clustering.hpp"
 
+void print_cluster_with_label(graph& g);
 bool has_empty_cluster(graph& g);
 
 clustering::clustering(vector<double>& _WkXY_sum, int _K, double _WXY_sum,int _xNum, vector<vector<int>>& _cluster_label){
@@ -18,28 +19,33 @@ double clustering::Norm(vector<double>& array ){
 	return(sqrt(Sum));
 }
 
-void clustering::begin_clustering(graph &g, vector<graph>& subgraph){
-    vector<vector<double>> pi(K);
+vector<double> clustering::get_generating_probability(){
     vector<double> p;
-    vertex_iterator i,j;
     for(int clusterNum = 0; clusterNum < K; clusterNum++){
         p.push_back(1.0*WkXY_sum[clusterNum]/WXY_sum);
-        //if(isnan(p[clusterNum]))cout << "p[" << clusterNum << "]:"<<  p[clusterNum] << endl;
     }
-
-    for(int z = 0; z < K; z++){
-        double tmp_p = 0;
-        for (boost::tie(i, j) = vertices(g); g[*i].int_descriptor < xNum; i++) {
-            for (auto e = in_edges(*i, g); e.first!=e.second; e.first++) {
-                //if(isnan(subgraph[z][source(*e.first, g)].ry))cout << subgraph[z][source(*e.first, g)].ry << endl;
-                tmp_p += g[*e.first].weight * subgraph[z][target(*e.first, g)].rx *subgraph[z][source(*e.first, g)].ry * p[z];
-            }
+    return p;
+}
+vector<double> clustering::calc_conditional_distribution(const graph& g, const vector<graph>& subgraph,const vector<double> p){
+        vertex_iterator i,j;
+        vector<double> conditional_p;
+        for(int z = 0; z < K; z++){
+            double tmp_p = 0;
+            for (boost::tie(i, j) = vertices(g); g[*i].int_descriptor < xNum; i++) {
+                for (auto e = in_edges(*i, g); e.first!=e.second; e.first++) {
+                    //if(isnan(subgraph[z][source(*e.first, g)].ry))cout << subgraph[z][source(*e.first, g)].ry << endl;
+                    tmp_p += g[*e.first].weight * subgraph[z][target(*e.first, g)].rx *subgraph[z][source(*e.first, g)].ry * p[z];
+                }
         }
-        p[z] = tmp_p/WXY_sum;
+        conditional_p.push_back(tmp_p/WXY_sum);
         //if(isnan(p[z]))cout << "p[" << z << "]:"<<  p[z] << endl;
     }
+    return conditional_p;
+}
 
-    // calc pi
+vector<vector<double>> clustering::calc_pi_using_bayesian_rule(const graph& g, const vector<graph>& subgraph,const vector<double> p){
+    vertex_iterator i,j;
+    vector<vector<double>> pi = vector<vector<double>>(K);
     for (boost::tie(i, j) = vertices(g); g[*i].int_descriptor < xNum; i++) {
         double tmp_sum = 0;
         for(int l = 0; l < K; l++){
@@ -53,23 +59,29 @@ void clustering::begin_clustering(graph &g, vector<graph>& subgraph){
             double val = 0;
             if(tmp_sum != 0)val = subgraph[z][*i].conditional_rank * p[z]/tmp_sum;
             pi[z].push_back(val);
-            //cout << "pi[" << z << "]:"<<  val << endl;
         }
     }
+    return pi;
+}
 
-    vector<vector<double>> s(xNum);
+vector<vector<double>> clustering::get_K_dimentional_vector(const vector<vector<double>>& pi){
+    vector<vector<double>> s = vector<vector<double>>(xNum);
     for(int i = 0; i < xNum; i++){
         for(int k = 0; k < K; k++){
             s[i].push_back(pi[k][i]);
         }
     }
-    
+    return s;
+}
+
+vector<vector<double>> clustering::get_center_vector(const graph& g, const vector<vector<double>>& s){
     vector<vector<double>> center_vec;
     center_vec = vector<vector<double>>(K,vector<double>(K,0));
 
     for( int Xk = 0; Xk < K; Xk++){
         int cluster_size = cluster_label[Xk].size();
         for(int col = 0; col < K; col++){
+            vertex_iterator i,j;
             for (boost::tie(i, j) = vertices(g); g[*i].int_descriptor < xNum; i++) {
                 if(g[*i].cluster_label == Xk){
                     center_vec[Xk][col] += s[*i][col];
@@ -78,55 +90,63 @@ void clustering::begin_clustering(graph &g, vector<graph>& subgraph){
             center_vec[Xk][col] /= cluster_size;
         }
     }
-    
-    // 距離を格納するリスト
-    vector<vector<double> > D;
-	D = vector<vector<double>>(xNum,vector<double>(K,0));
-    vertex_iterator m,n;
+    return center_vec;
+}
 
-    vector<vector<int>> new_cluster_label(K);
-        
-    for (boost::tie(m, n) = vertices(g); *m < xNum; m++) {
-    //Dの計算
-        double norms = Norm(s[*m]);
-        int index = 0;
+double clustering::calc_distance_by_one_minus_cosine_similarity(vector<double> s_x, vector<vector<double>>center_vec, int k){
+    double tmp = 0;
+    for (int l = 0; l < K; l++){
+        tmp += s_x[l] * center_vec[k][l];
+    }
+    double cosine_similarity = 1.0 - (tmp/(Norm(s_x) * Norm(center_vec[k])));
+    return cosine_similarity;
+}
+
+int clustering::get_index_of_nearest_cluster(vector<double> s_x,const vector<vector<double>> center_vec){
+    vector<double> D;
+        int index = -1;
         double minDis = 1;
         for (int k = 0; k < K; k++){
-            double tmp = 0;
-            for (int l = 0; l < K; l++){
-                tmp += s[*m][l] * center_vec[k][l];
-            }
-            D[*m][k] = 1.0 - (tmp/(norms * Norm(center_vec[k])));
-    //assign
-            //cout <<  *m << "->" << k << ": " << D[*m][k] << endl;
-            if(D[*m][k] < minDis){
-                minDis = D[*m][k];
+            D.push_back(calc_distance_by_one_minus_cosine_similarity(s_x,center_vec, k));
+            if(D[k] < minDis){
+                minDis = D[k];
                 index = k;
             }
         }
+    return index;
+}
 
-        if(g[*m].cluster_label == index){
-            g[*m].same_previous_cluster = true;
+void clustering::update_cluster_label(vertex_property v, const int index){
+    if(v.cluster_label == index){
+            v.same_previous_cluster = true;
         }else{
-            g[*m].same_previous_cluster = false;
+            v.same_previous_cluster = false;
         }
-        g[*m].cluster_label = index;
-        new_cluster_label[index].push_back(*m);
+        v.cluster_label = index;
     }
-    cluster_label = new_cluster_label;
 
-    // for(int i = 0; i < K; i++){
-    //     cout << "cluster = { ";
-    //     for(int j = 0; j < cluster_label[i].size(); j++){
-    //         cout << cluster_label[i][j] << ", "<< flush;
-    //     }
-    //     cout << "}" <<endl;
-    // }
-
-    void print_cluster_with_label(graph& g);
+void clustering::check_empty_cluster(graph & g){
     if(has_empty_cluster(g)){
         cout << "Cluster became empty , you have to run the program again." << endl;
         print_cluster_with_label(g);
         exit(0);
     }
+}
+
+void clustering::begin_clustering(graph &g, vector<graph>& subgraph){
+    auto p = get_generating_probability();
+    auto conditional_p = calc_conditional_distribution(g, subgraph, p);
+    auto pi = calc_pi_using_bayesian_rule(g, subgraph, conditional_p);
+
+    auto s = get_K_dimentional_vector(pi);
+    auto center_vec = get_center_vector(g, s);
+    
+    vertex_iterator m,n;
+    vector<vector<int>> new_cluster_label(K);
+    for (boost::tie(m, n) = vertices(g); *m < xNum; m++) {
+        int index = get_index_of_nearest_cluster(s[*m], center_vec);
+        update_cluster_label(g[*m], index);
+        new_cluster_label[index].push_back(*m);
+    }
+    cluster_label = new_cluster_label;
 }
